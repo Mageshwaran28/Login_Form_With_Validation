0924ffc548950313dd6489dc60e69bb48151a674
Mageshwaran <123348992+Mageshwaran28@users.noreply.github.com>
2023-08-21 10:28:01 +0530

    Validation Updated

--- Login_Form_Validation.js
+++ Login_Form_Validation.js
@@ -1,47 +1,168 @@
+function get(ele){
+    return document.querySelector(ele);
+}
 
-var email = document.querySelector('.email');
-var pwd = document.querySelector('.password');
-var signin = document.querySelector('.signin');
-var google = document.querySelector('.google-signin-group');
-var forgot = document.querySelector('.forgot');
-var signup = document.querySelector('.signup-group');
-var result = document.querySelector('.result');
-var error = document.querySelector('.error');
-var closeIcon = document.querySelector('.result i');
+let email = get('.email');
+let pwd = get('.password');
+let signin = get('.signin');
+let google = get('.google-signin-group');
+let forgot1 = get('.forgot');
+let signup = get('.signup-group');
+let result1 = get('.result');
+let error = get('.error');
+let closeIcon = get('.result i');
+let res1 = get('.signin-group > span');
+let emailError = '';
+let pwdError = '';
+const regEx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-signin.addEventListener('click', ()=>{
-    event.preventDefault();
-    console.log('Hello this is my first validation in zerp labs organization')
 
-    var emailVal = email.value;
-    var passwordVal = pwd.value;
-    console.log(passwordVal)
-
-    var regEx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  
-
-    if(emailVal==''  || regEx.test(emailVal)==false){
-        resultPopup("result failure", "Invalid email" , "fa-solid fa-xmark");
+const users =[
+    {
+        email:'vignesh.t@zirius.in',
+        pwd:'Vignesh@123'
+    },
+    {
+        email: 'davidjrmeshach@gmail.com',
+        pwd: '#David123'
+    },
+    {
+        email: 'rajasaravananmcsea@gmail.com',
+        pwd: 'Raja123#'
+    },
+    {
+        email: 'mageshwaranbalaji25@gmail.com',
+        pwd: 'Eshwaran@123'
+    },
+    {
+        email: 'harsha@zirius.in',
+        pwd: 'Harsha$123'
     }
-    else if(passwordVal==''){
-        resultPopup("result failure", "Invalid password","fa-solid fa-xmark");
-    }
-    else if(passwordVal.length<6){
-        resultPopup("result failure", "Password min 6 character","fa-solid fa-xmark");
-    }
-    else if(/[a-z]/.test(passwordVal)==false || /[A-Z]/.test(passwordVal)==false || /[0-9]/.test(passwordVal)==false){
-        resultPopup("result failure", "Password must be strong","fa-solid fa-xmark");
-    }
-    else{
-        resultPopup("result success", "Logged in successfully","fa-solid fa-check");
-    }
+]
 
+let emailVal="";
+let passwordVal="";
+email.addEventListener('input', (event) => {
+    emailVal = event.target.value;
+
+    if(emailError=='Empty' && emailVal!=""){
+        email.parentNode.className = "input-group email-group";
+        emailError='';
+    }
+    else if(emailError=='Invalid' && regEx.test(emailVal)==true){
+        email.parentNode.className = "input-group email-group";
+        emailError='';
+    }
 })
 
+pwd.addEventListener('input', (event) => {
+    passwordVal = event.target.value;
+    
+    if((pwdError=='Empty' && passwordVal!="") || 
+        (pwdError=='Invalid' && passwordVal.length>=6) || 
+        (pwdError=='lowercase' && /[a-z]/.test(passwordVal)==true) || 
+        (pwdError=='uppercase' && /[A-Z]/.test(passwordVal)==true) || 
+        (pwdError=='number' && /[0-9]/.test(passwordVal)==true) || 
+        (pwdError=='special' && /[$@!%*#?&]/.test(passwordVal)==true)){
+        pwd.parentNode.className = "input-group password-group";
+        pwdError='';
+    }
+})
+
+function emailValidation(){
+    let errorMsg='';
+
+    if(emailVal==''){
+        errorMsg = "Provide a email";
+        emailError = 'Empty'
+    }
+    else if(regEx.test(emailVal)==false){
+        errorMsg = "Provide a properly formatted email";
+        emailError = 'Invalid'
+    }
+
+    if(errorMsg==''){
+        email.parentNode.className = "input-group email-group success";
+        email.nextElementSibling.innerHTML = errorMsg;
+        return true;  
+    }
+    else{
+        email.parentNode.className = "input-group email-group error";
+        email.nextElementSibling.innerHTML = errorMsg;
+        return false;
+    }
+}
+
+function pwdValidation(){
+    let errorMsg='';
+    if(passwordVal==''){
+        errorMsg = "Provide a password";
+        pwdError = 'Empty'
+    }
+    else if(passwordVal.length<6){
+        errorMsg = "Password min 6 char";
+        pwdError = 'Invalid';
+    }
+    else if(/[a-z]/.test(passwordVal)==false ){ 
+        errorMsg = "Password min 1 lowercase";
+        pwdError = 'lowercase';
+    }
+    else if(/[A-Z]/.test(passwordVal)==false){
+        errorMsg = "Password min 1 uppercase";
+        pwdError = 'uppercase';
+    }
+    else if(/[0-9]/.test(passwordVal)==false){
+        errorMsg = "Password min 1 number";
+        pwdError = 'number';
+    }
+    else if(/[$@!%*#?&]/.test(passwordVal)==false){
+        errorMsg = "Password min 1 special char";
+        pwdError ='special';
+    }
+
+    if(errorMsg==''){
+        pwd.parentNode.className = "input-group password-group success";
+        pwd.nextElementSibling.innerHTML = errorMsg;
+        return true;
+    }
+    else{
+        pwd.parentNode.className = "input-group password-group error";
+        pwd.nextElementSibling.innerHTML = errorMsg;
+        return false;
+    }
+
+   
+
+}
+
+signin.addEventListener('click', (event)=>{
+    event.preventDefault();
+    let bool = emailValidation();
+    let bool1 = pwdValidation();
+
+    if(bool && bool1){
+        res1.style.display = 'block';
+        for(let user of users){
+            if(emailVal==user.email && passwordVal==user.pwd){
+                res1.innerHTML = 'Logged in successfully'
+                res1.className = 'res';
+                return;
+            }
+        }
+        res1.className='res error'
+        res1.innerHTML = 'Invalid credentials'
+    }
+    else{
+        res1.style.display='none';
+    }
+})
+
+
+
 function resultPopup( type , err , icon){
-    result.style.display = 'flex';
+    result1.style.display = 'flex';
     closeIcon.className = icon;
-    result.className = type;
+    result1.className = type;
     error.innerHTML = err;
 
     setTimeout(resultClose , 3000);
@@ -53,7 +174,7 @@ google.addEventListener('click',()=>{
     resultPopup("result process","","fa-solid fa-rotate")
 })
 
-forgot.addEventListener('click',()=>{
+forgot1.addEventListener('click',()=>{
     resultPopup("result process","","fa-solid fa-rotate")
 })
 
@@ -62,6 +183,6 @@ signup.addEventListener('click',()=>{
 })
 
 function resultClose(){
-    result.style.display = 'none';
-    result.className = 'result';
+    result1.style.display = 'none';
+    result1.className = 'result';
 }
